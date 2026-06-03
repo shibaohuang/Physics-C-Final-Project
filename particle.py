4274fbadca363749a15cdd7a70a3d5c1c722baaf
Skylar Huang <shibaoh@nycstudents.net>
2026-06-03 01:04:53 -0400

    add graph + scuffed simulator cuz particles are leaving the torus

--- particle.py
+++ particle.py
@@ -36,20 +36,52 @@ v2 = vector(-1,-1,-0.5)
 
 mass = 1.0
 charge = 1.0
-B_field = vector(0, 0, 1)  
+B_field = vector(0, 0, 4)  
 pos_init = vector(0, 0, 0)
 
 dt = 0.01
 t = 0
+
+g1 = graph(
+    title="Fusion Energy vs Time",
+    xtitle="Time",
+    ytitle="Energy (MeV)"
+)
+
+energy_curve = gcurve(color=color.yellow)
+
+g2 = graph(
+    title="Collision Count vs Time",
+    xtitle="Time",
+    ytitle="Collisions"
+)
+
+collision_curve = gcurve(color=color.green)
 while True:
-    rate(1000)
-    if running:
-        F1 = charge1 * cross(v_init1, B_field)
-        F2 = charge2 * cross(v_init2, B_field)
-        v_init1 = v_init1 + (F1 / mass1) * dt
-        v_init2 = v_init2 + (F2 / mass2) * dt
-        particle1.pos = particle1.pos + v_init1 * dt
-        trail1.append(particle1.pos)
-        particle2.pos = particle2.pos + v_init2 * dt
-        trail2.append(particle2.pos)
-        t = t + dt
+    rate(500)
+    F1 = charge * cross(v1,B_field)
+    F2 = charge * cross(v2,B_field)
+    v1 += (F1/mass)*dt
+    v2 += (F2/mass)*dt
+    p1.pos += v1*dt
+    p2.pos += v2*dt
+    if mag(p1.pos) > 8:
+        p1.pos = vector(-3,0,0)
+    if mag(p2.pos) > 8:
+        p2.pos = vector(3,0,0)
+    if mag(p1.pos - p2.pos) < fusion_distance:
+        collision_count += 1
+        total_energy += fusion_energy
+        flash = sphere(
+            pos=(p1.pos+p2.pos)/2,
+            radius=0.4,
+            color=color.yellow,
+            emissive=True
+        )
+        p1.pos = vector(-3,0,0)
+        p2.pos = vector(3,0,0)
+        v1 = vector(1,1,0.5)
+        v2 = vector(-1,-1,-0.5)
+    energy_curve.plot(t,total_energy)
+    collision_curve.plot(t,collision_count)
+    t += dt
